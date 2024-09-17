6a1e44eaffc71bd689c2d75c1451d2031e1ccdcb
lukauskokovic <gilepapak@gmail.com>
2024-09-18 00:04:58 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chooseButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fullPathLabel = new System.Windows.Forms.Label();
+            this.installDirectoryLabel = new System.Windows.Forms.Label();
+            this.installingLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.exitButton.Location = new System.Drawing.Point(324, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 28);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.nextButton);
+            this.panel1.Controls.Add(this.exitButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 340);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 28);
+            this.panel1.TabIndex = 1;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.nextButton.Location = new System.Drawing.Point(249, 0);
+            this.nextButton.Margin = new System.Windows.Forms.Padding(3, 3, 120, 3);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 28);
+            this.nextButton.TabIndex = 1;
+            this.nextButton.Text = "Install";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Install Directory";
+            // 
+            // chooseButton
+            // 
+            this.chooseButton.Location = new System.Drawing.Point(114, 4);
+            this.chooseButton.Name = "chooseButton";
+            this.chooseButton.Size = new System.Drawing.Size(75, 23);
+            this.chooseButton.TabIndex = 4;
+            this.chooseButton.Text = "Choose...";
+            this.chooseButton.UseVisualStyleBackColor = true;
+            this.chooseButton.Click += new System.EventHandler(this.chooseButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Full Path:";
+            // 
+            // fullPathLabel
+            // 
+            this.fullPathLabel.AutoSize = true;
+            this.fullPathLabel.BackColor = System.Drawing.Color.Transparent;
+            this.fullPathLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fullPathLabel.Location = new System.Drawing.Point(12, 113);
+            this.fullPathLabel.Name = "fullPathLabel";
+            this.fullPathLabel.Size = new System.Drawing.Size(46, 18);
+            this.fullPathLabel.TabIndex = 6;
+            this.fullPathLabel.Text = "label3";
+            // 
+            // installDirectoryLabel
+            // 
+            this.installDirectoryLabel.AutoSize = true;
+            this.installDirectoryLabel.Location = new System.Drawing.Point(12, 39);
+            this.installDirectoryLabel.Name = "installDirectoryLabel";
+            this.installDirectoryLabel.Size = new System.Drawing.Size(35, 13);
+            this.installDirectoryLabel.TabIndex = 7;
+            this.installDirectoryLabel.Text = "label3";
+            // 
+            // installingLabel
+            // 
+            this.installingLabel.AutoSize = true;
+            this.installingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.installingLabel.Location = new System.Drawing.Point(12, 203);
+            this.installingLabel.Name = "installingLabel";
+            this.installingLabel.Size = new System.Drawing.Size(126, 29);
+            this.installingLabel.TabIndex = 8;
+            this.installingLabel.Text = "Installing...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 368);
+            this.Controls.Add(this.installingLabel);
+            this.Controls.Add(this.installDirectoryLabel);
+            this.Controls.Add(this.fullPathLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chooseButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Toggl Windows App Installer";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button chooseButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label fullPathLabel;
+        private System.Windows.Forms.Label installDirectoryLabel;
+        private System.Windows.Forms.Label installingLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace installer
+{
+    public partial class Form1 : Form
+    {
+        private bool installing = false;
+        private const string AppName = "TogglWindowsApp";
+        private string _installDir = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+        private CancellationTokenSource _cancelSource;
+        public Form1()
+        {
+            InitializeComponent();
+            SetInstallDirectory(_installDir);
+            installingLabel.Visible = false;
+        }
+
+        private string FullFolderPath(string dir) => Path.Combine(dir, AppName);
+
+        private void SetInstallDirectory(string installDir)
+        {
+            bool exists;
+            try
+            {
+                exists = Directory.Exists(installDir);
+            }
+            catch
+            {
+                exists = false;
+            }
+            if (!exists)
+            {
+                MessageBox.Show("Folder does not exist");
+                return;
+            }
+            _installDir = installDir;
+            installDirectoryLabel.Text = _installDir;
+            fullPathLabel.Text = FullFolderPath(_installDir);
+            FormClosing += (s, args) => {
+                _cancelSource?.Cancel();
+            };
+        }
+
+        private void chooseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+
+            SetInstallDirectory(dialog.SelectedPath);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            if (installing)
+            {
+                var result = MessageBox.Show("The software is installing are you sure?", "Please confirm", MessageBoxButtons.YesNo);
+                if (result != DialogResult.Yes) return;
+            }
+            this.Close();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("The selected FULL PATH will be cleared before installing are you sure you want to continue?", "Please confirm", MessageBoxButtons.YesNo);
+            if (result != DialogResult.Yes) return;
+            installing = true;
+            nextButton.Enabled = false;
+            _cancelSource = new CancellationTokenSource();
+            installingLabel.Visible = true;
+            Task.Run(InstallTask, _cancelSource.Token);
+        }
+
+        private void InstallTask()
+        {
+            try
+            {
+                var path = FullFolderPath(_installDir);
+                //var backendExePath = Path.Combine(path, "cmsmonitor.exe");
+                /* var key = Registry.ClassesRoot.CreateSubKey("cmsmonitor");
+                key.SetValue("URL Protocol", "");
+                key.SetValue("", "Monitoring software");
+                var shellKey = key.CreateSubKey("shell");
+                var openKe = shellKey.CreateSubKey("open");
+                var commandKey = openKe.CreateSubKey("command");
+                commandKey.SetValue("", $"\"{backendExePath}\" \"%1\"");*/
+
+                var bytes = Properties.Resources.install;
+                MemoryStream stream = new MemoryStream(bytes);
+                ZipArchive archive = new ZipArchive(stream);
+
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+                Directory.CreateDirectory(path);
+                archive.ExtractToDirectory(path);
+                File.Copy(Path.Combine(path, "TogglAutomationApp.exe"), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "TogglAutomationApp.exe"));
+                Invoke(new Action(() => {
+                    installingLabel.Text = "Installed!";
+                }));
+                installing = false;
+                MessageBox.Show("Software installed");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("ERROR INSTALLING SOFTWARE: " + ex.ToString());
+                //
+            }
+        }
+    }
+}
